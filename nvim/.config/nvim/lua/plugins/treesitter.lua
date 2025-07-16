4f91e8f74458af923d9061d1e409ff4d76dc19d9
felrfn <ardiansyahrafael5@gmail.com>
2025-07-16 03:08:59 -0400

    feat: add treesitter

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+
+return {
+  'nvim-treesitter/nvim-treesitter',
+  lazy = true,
+  config = function()
+    require'nvim-treesitter.configs'.setup {
+      highlight = {
+        enable = true,        },
+
+      indent = {
+        enable = true,
+      },
+
+      ensure_installed = { 
+        "bash", "lua", "python", "javascript", "html", "css", "markdown", "go",
+      },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>", 
+          node_incremental = "<C-space>",
+	  node_decremental = "<bs>",
+        },
+      },
+    }
+  end,
+  build = ':TSUpdate',
+}
+
