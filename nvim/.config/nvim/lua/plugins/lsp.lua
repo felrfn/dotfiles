dd5dfa3888a9d6f3ce34ccb13c2d316aedc65880
felrfn <ardiansyahrafael5@gmail.com>
2025-07-17 15:36:35 +0700

    chore: update lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,18 +1,25 @@
 return {
-	"mason-org/mason-lspconfig.nvim",
-	opts = {
-		ensure_installed = { "lua_ls", "gopls" },
-	},
-	dependencies = {
-		{ "mason-org/mason.nvim", opts = {} },
-		{
-			"neovim/nvim-lspconfig",
-			config = function()
-				require'lspconfig'.lua_ls.setup{}
-				vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-				vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-				vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-			end
-		},
-	}
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = { "lua_ls", "gopls", "ts_ls" },
+  },
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    {
+      "neovim/nvim-lspconfig",
+      config = function()
+        require'lspconfig'.lua_ls.setup({})
+        require'lspconfig'.gopls.setup({})
+        require'lspconfig'.ts_ls.setup({
+          settings = { update_in_insert =true, }
+        })
+
+
+        vim.diagnostic.config({ virtual_text = true })
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+        vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+      end
+    },
+  }
 }
