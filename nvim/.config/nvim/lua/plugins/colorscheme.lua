1786d2e2db15bd6b749e1eb838e492bbed0b3afb
felrfn <ardiansyahrafael5@gmail.com>
2025-07-21 12:09:16 +0700

    add: remove number's bg

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,17 @@
 return {
-  "rebelot/kanagawa.nvim", name = "kanagawa", priority = 100,
+  "rebelot/kanagawa.nvim",
+  name = "kanagawa",
+  priority = 100,
   config = function()
     require("kanagawa").setup({
-	transparent = true,
-	theme = "dragon",
+      transparent = true,
+      theme = "dragon",
     })
-    vim.cmd.colorscheme "kanagawa"
-    end
-}
 
+    vim.cmd([[
+  colorscheme kanagawa
+  "rm bg on line numbers
+  highlight LineNr guibg=NONE ctermbg=NONE
+]])
+  end,
+}
